9059e5a3dc82e134974ba88cbf011f0ad4c6f989
JessicaChris <jessica3chris@gmail.com>
2025-09-01 19:55:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import AboutUs from "./pages/Aboutus";
+import Gallery from "./pages/Gallery";
+import Blog from "./pages/Blog";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
   );
